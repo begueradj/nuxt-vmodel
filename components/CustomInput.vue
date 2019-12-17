26e4d1bd1943379f034b4b9296c2425f81d9c9d3
begueradj <begueradj.bill@gmail.com>
2019-12-17 09:41:47 +0200

    Add custom input

---
+++ components/CustomInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-text-field
+    v-bind:value="value"
+    v-on:input="$emit('input', value)"
+  />
+</template>
+
+<script>
+export default {
+  name: 'CustomInput',
+  props: {
+    value: {
+      type: String,
+      required: true
+    }
+  },
+  watch: {
+    value () {
+      this.$emit('input', this.value)
+    }
+  }
+}
+</script>
